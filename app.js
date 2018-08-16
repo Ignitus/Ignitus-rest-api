79c5861970b571aaebe19fee84f0aa5a9eacd856
Nikhil Ranjan <niklabh811@gmail.com>
2018-08-16 09:29:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  // res.render('error');
+ // res.render('error');
 });
 
 module.exports = app;
