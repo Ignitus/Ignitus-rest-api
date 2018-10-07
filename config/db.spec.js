c564ad33be3e055cb0abe5973c76fa7b47d887a0
Parul Gupta <parulgupta26@users.noreply.github.com>
2018-10-07 23:00:31 +0530

    Parulgupta26/add istanbul (#35)
    * Added istanbul coverage
    
    * fixed gitignore and test all files with ext spec.js
    
    * Added mocha unit test and istanbul coverage

---
+++ config/db.spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Array -- This is a specimen. Please add a valid test', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3, 4].indexOf(5), -1);
+    });
+  });
+});
