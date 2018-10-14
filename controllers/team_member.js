083010106ca1aa68cddd929fdd4c8bfa08620094
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-10-14 13:03:56 -0400

    fixed linting errors in TeamMember controllers, fixed update TeamMember method and typo, added the use of a gitignored config file to connext app to mlab, updated contribution.md to reflect that update

--- controllers/team_member.js
+++ controllers/team_member.js
@@ -1,37 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // eslint-disable-line 
 
-const teamMemberProfile = require('../models/team_member').teamMemberProfile;
+const { teamMemberProfile } = require('../models/team_member');
 const responseHandler = require('../helper/responseHandler');
 
 // fetch all TeamMembers
 
-exports.getAllTeamMembers = function (req, res) {
-  teamMemberProfile
-    .find({}, (err, docs) => {
-      if (err) {
-        return responseHandler.error(res);
-      }
+exports.getAllTeamMembers = (req, res) => {
+  teamMemberProfile.find({}, (err, docs) => {
+    if (err) {
+      return responseHandler.error(res);
+    }
 
-      return responseHandler.success(res, docs)
-    })
+    return responseHandler.success(res, docs);
+  });
 };
 
 // fetch single TeamMember by Id
 
-exports.getTeamMember = function (req, res) {
+exports.getTeamMember = (req, res) => {
   teamMemberProfile.findById(req.params.id, (err, teamMember) => {
     if (err) {
-      return responseHandler.error(res, 'TeamMember not found', 404)
+      return responseHandler.error(res, 'TeamMember not found', 404);
     }
 
-    return responseHandler.success(res, teamMember)
-  })
-
+    return responseHandler.success(res, teamMember);
+  });
 };
 
 // create a Team Member
 
-exports.createTeamMember = function (req, res) {
+exports.createTeamMember = (req, res) => {
   teamMemberProfile
     .create(req.body)
     .then(teamMember => responseHandler.success(res, teamMember))
@@ -40,37 +38,17 @@ exports.createTeamMember = function (req, res) {
 
 // update/edit a TeamMember
 
-exports.updateTeamMember = function (req, res) {
-  teamMemberProfile.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, docs) => {
-      if (err || !docs) {
-        return err
-          ? responseHandler.errpr(res)
-          : responseHandler.error(res, 'TeamMember not found', 404);
-      }
-
-      return responseHandler.success(res, docs);
-    }
-  );
+exports.updateTeamMember = (req, res) => {
+  teamMemberProfile
+    .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(updatedTeamMember => responseHandler.success(res, updatedTeamMember))
+    .catch(() => responseHandler.error(res, 'Failed to update TeamMember', 404));
 };
 
 // delete a TeamMember
 
-exports.deleteTeamMember = function (req, res) {
-  teamMemberProfile.findByIdAndRemove(req.params.id, (err, doc) => {
-    if (err || !doc) {
-      return err
-        ? responseHandler.error(res)
-        : responseHandler.error(
-          res,
-          'TeamMember not found, failed to delete',
-          404
-        );
-    }
-
-    return responseHandler.success(res, { _id: doc._id });
-  });
+exports.deleteTeamMember = (req, res) => {
+  teamMemberProfile.findByIdAndRemove(req.params.id)
+    .then(responseFromDeleteQuery => responseHandler.success(res, responseFromDeleteQuery))
+    .catch(() => responseHandler.error(res, 'Failed to delete TeamMember', 404));
 };
