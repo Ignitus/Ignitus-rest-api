4349455f102ba64eeb9547dc11cc576294672cc3
Pradnya <pradnya24.dalvi@gmail.com>
2019-02-27 17:57:55 +0530

    Add test files

--- tests/professorModel.spec.js
+++ tests/professorModel.spec.js
@@ -1,14 +1,45 @@
 var expect = require('chai').expect;
- 
+var assert = require('assert');
+
 var professorModel = require('../models/professor_profile');
- 
+
 describe('Professor Model', function() {
     it('should be invalid if email is empty', function(done) {
         var p =  new professorModel.professorProfile();
- 
+
         p.validate(function(err) {
             expect(err.errors.email).to.exist;
             done();
         });
     });
+
+    it('should be invalid if name is empty', function(done) {
+        var p =  new professorModel.professorProfile();
+
+        p.validate(function(err) {
+            expect(err.errors.name).to.exist;
+            done();
+        });
+    });
+
+    it('should be invalid if year_passed is empty', function(done) {
+        var p =  new professorModel.professorProfile();
+
+        p.validate(function(err) {
+            assert.equal("2000".length,4);
+            done();
+        });
+    });
+
+    it('should be invalid if properties are empty', function(done) {
+        var p =  new professorModel.professorProfile();
+
+        p.validate(function(err) {
+            expect(err.errors.email).to.exist;
+            expect(err.errors.name).to.exist;
+            expect(err.errors.research_fields).to.exist;
+          done();
+        });
+    });
+
 });
--- tests/studentModel.spec.js
+++ tests/studentModel.spec.js
@@ -1,14 +1,47 @@
 var expect = require('chai').expect;
- 
+var assert = require('assert');
+
 var studentModel = require('../models/student_profile');
- 
+
 describe('Student Model', function() {
     it('should be invalid if email is empty', function(done) {
         var s =  new studentModel.studentProfile();
- 
+
         s.validate(function(err) {
             expect(err.errors.email).to.exist;
             done();
         });
     });
+
+    it('should be invalid if name is empty', function(done) {
+        var p =  new studentModel.studentProfile();
+
+        p.validate(function(err) {
+            expect(err.errors.name).to.exist;
+            done();
+        });
+    });
+
+    it('should be invalid if year_passed is empty', function(done) {
+        var p =  new studentModel.studentProfile();
+
+        p.validate(function(err) {
+            assert.equal("2000".length,4);
+            done();
+        });
+    });
+
+
+    it('should be invalid if properties are empty', function(done) {
+        var p =  new studentModel.studentProfile();
+
+        p.validate(function(err) {
+            expect(err.errors.email).to.exist;
+            expect(err.errors.name).to.exist;
+            expect(err.errors.resume).to.exist;
+          done();
+        });
+    });
+
+
 });
