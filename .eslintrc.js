cade1d9ff1e51518bf24e66b83e53f311c87eb20
akshataw <wagheakshu@gmail.com>
2019-03-01 10:28:11 +0530

    fix: liniting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,18 @@ module.exports = {
         "mocha": true
     },
     "rules": {
-      "strict": 0,
+      "arrow-body-style": 0,
+      "brace-style": 0,
+      "camelcase": 0,
+      "consistent-return": 0,
+      "func-names": 0,
+      "new-cap": 0,
+      "no-console": 0,
+      "no-shadow": 0,
       "no-tabs": 0,
       "no-underscore-dangle": 0,
       "no-unused-expressions": 0,
-      "consistent-return": 0,
-      "new-cap": 0,
-      "arrow-body-style": 0,
       "no-unused-vars": 0,
-      "no-shadow": 0,
-      "camelcase": 0,
-      "brace-style": 0,
-      "func-names": 0,
-      "no-console": 0
+      "strict": 0,
     }
 };
--- app.js
+++ app.js
@@ -84,23 +84,23 @@ app.use('/', users);
 app.use('/', internships);
 app.use('/', testimonial);
 app.use('/', teamMember);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
-});
+//
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use((err, req, res) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   // res.render('error');
+// });
 
 module.exports = app;
