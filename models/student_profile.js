590a9a4de844ce9c394689580644ee7d1d4ab81d
pradnyasd <46742046+pradnyasd@users.noreply.github.com>
2019-03-01 16:42:06 +0530

    Add test files (#63)
    * Add test files
    
    * Removed Duplicate test

--- models/student_profile.js
+++ models/student_profile.js
@@ -2,33 +2,45 @@ const mongoose = require('mongoose');
 
 const studentProfileSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+
   email: {
     type: String,
     required: true,
     unique: true,
     match: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
   },
-  name: { type: String, match: /^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/ },
+
+  name: { type: String, required: true, match: /^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/ },
+
   bio: { type: String },
+
   address: { type: String },
+
   social_links: {
     facebook: { type: String },
     github: { type: String },
     linkedin: { type: String },
     google_plus: { type: String },
   },
+
   skill: [String],
+
   about: {
     current_job: { type: String },
     looking_for: { type: String },
   },
+
   profile_pic: { type: String },
-  resume: { type: String },
-  education: [{
-    college_name: { type: String },
-    position: { type: String },
-    year_passed: { type: Number },
-  }],
+
+  resume: { type: String, required: true, },
+
+  education: {
+    college_name: { type: String, required: true,},
+    position: { type: String, required: true,},
+    year_passed: { type: Number, required: true, }
+  },
+
+
   experience: [{
     company_name: { type: String },
     position: { type: String },
