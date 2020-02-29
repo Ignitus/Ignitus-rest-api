90f596b328cf973ee18782ff6b0778acaaae5fc8
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2020-02-29 20:32:58 +0100

    Update db.js

--- api/Configuration/db.js
+++ api/Configuration/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 module.exports = function connectDB() {
   return mongoose.connect(
-    'mongodb://ignitus:ignitus001@ds046037.mlab.com:46037/ignitus',
+    process.env.DATABASE_URI,
   );
 };
