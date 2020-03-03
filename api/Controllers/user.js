239f72be72645f45ece83cda1b3f35f0de1fb923
Rawat <divyanshu.r46956@gmail.com>
2020-03-03 21:25:28 +0100

    Module  🔨

--- api/Controllers/user.js
+++ api/Controllers/user.js
@@ -14,17 +14,9 @@ const Linkedin = require('node-linkedin')(
   process.env.LINKEDIN_APP_ID,
   process.env.LINKEDIN_SECRET,
 );
-const nodemailer = require('nodemailer');
+
 const responseHandler = require('../Utils/responseHandler');
 const config = require('../Configuration/config');
-
-const smtpTransport = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
 
 /*
  * Feature Reuest Update, Not Needed atm.
