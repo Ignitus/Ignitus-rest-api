af979146fcc2413d3d4b6c12297cee7ea45a36e3
Rawat <divyanshu.r46956@gmail.com>
2020-03-07 01:37:38 +0100

    Migrated user-route, disabled certain fn atm.

--- api/Routes/users.js
+++ api/Routes/users.js
@@ -1,32 +1,44 @@
-const express = require('express');
+import express from 'express';
 
 const router = express.Router();
 const Users = require('../Controllers/user');
 
-router.post('/login', Users.login);
+import {
+  studentRegister,
+  professorRegister,
+  getUserInfoFromToken,
+  login
+} from '../Controllers/user.js';
 
-router.post('/register/student', Users.studentRegister);
-router.get('/student/oauth/linkedin', Users.studentlinkedlogin);
-router.get(
-  '/student/oauth/linkedin/callback',
-  Users.studentlinkedlogincallback,
-);
+router.post('/login', login);
+router.post('/register/student', studentRegister);
+router.post('/register/professor', professorRegister);
+router.post('/userinfofromtoken', getUserInfoFromToken);
 
-router.post('/register/professor', Users.professorRegister);
-router.get('/professor/oauth/linkedin', Users.professorlinkedlogin);
-router.get(
-  '/professor/oauth/linkedin/callback',
-  Users.professorlinkedlogincallback,
-);
+/* Disabled atm.
+    router.get('/student/oauth/linkedin', Users.studentlinkedlogin);
+    router.get(
+      '/student/oauth/linkedin/callback',
+      Users.studentlinkedlogincallback,
+    );
+*/
 
-/**
- * User verification disabled for now.
- * router.get('/verify', Users.verify);
- */
+/* Disabled atm.
+    router.get('/professor/oauth/linkedin', Users.professorlinkedlogin);
+    router.get(
+      '/professor/oauth/linkedin/callback',
+      Users.professorlinkedlogincallback,
+    );
+*/
 
-router.post('/forgotPassword', Users.forgotPassword);
-router.get('/reset', Users.resetPassword);
+/* Disabled atm.
+    router.get('/verify', Users.verify);
+*/
 
-router.put('/updatePasswordViaMail', Users.updatePassword);
-router.post('/userinfofromtoken', Users.getUserInfoFromToken);
+/* Disabled atm.
+  router.post('/forgotPassword', Users.forgotPassword);
+  router.get('/reset', Users.resetPassword);
+  router.put('/updatePasswordViaMail', Users.updatePassword);
+*/
+
 module.exports = router;
