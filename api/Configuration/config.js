14f306a2c1c46ba37a77994e2181a2ca1a16f175
Rawat <divyanshu.r46956@gmail.com>
2020-03-08 01:55:45 +0100

    Updated config ★

--- api/Configuration/config.js
+++ api/Configuration/config.js
@@ -6,4 +6,6 @@ module.exports = {
   privateVapidEmail: process.env.PVT_VAPID_EMAIL,
   publicVapidKey: process.env.PUBLIC_VAPID_KEY,
   privateVapidKey: process.env.PRIVATE_VAPID_KEY,
+  regularExpressionEmail: process.env.REGEX_EMAIL,
+  regularExpressionUserName: process.env.REGEX_USERNAME,
 };
