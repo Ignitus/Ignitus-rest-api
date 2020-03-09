c0adc0ea9ea94316e256c170d6a3d0cf3dfd1df5
Rawat <divyanshu.r46956@gmail.com>
2020-03-09 23:47:22 +0100

    Improved err handling 🔨

--- api/Controllers/testimonialController.js
+++ api/Controllers/testimonialController.js
@@ -3,21 +3,31 @@ import Testimonial from '../Models/testimonialModel.js';
 import responseHandler from '../Utils/responseHandler.js';
 
 export const addTestimonial = (req, res) => {
-  Testimonial.create(req.body, err =>
-    err ? responseHandler.error(res) : responseHandler.success(res)
-  );
+  Testimonial.create(req.body, err => {
+    if (err) {
+      throw new Error(err);
+    }
+    return responseHandler.success(res);
+  });
 };
 export const fetchAllTestimonial = (req, res) => {
-  Testimonial.find({}, (err, docs) =>
-    err ? responseHandler.error(res) : responseHandler.success(res, docs)
-  );
+  Testimonial.find({}, (err, docs) => {
+    if (err) {
+      throw new Error(err);
+    }
+    responseHandler.success(res, docs);
+  });
 };
 export const fetchTestimonialByID = (req, res) => {
-  Testimonial.findById(req.params.id, (err, docs) =>
-    !err && docs
-      ? responseHandler.success(res, docs)
-      : responseHandler.error(res, 'Testimonial not found', 404)
-  );
+  Testimonial.findById(req.params.id, (err, docs) => {
+    if (err) {
+      throw new Error(err);
+    }
+    if (!docs) {
+      return responseHandler.error(res, 'Testimonial not found!', 404);
+    }
+    return responseHandler.success(res, docs);
+  });
 };
 export const updateTestimonial = (req, res) => {
   Testimonial.findByIdAndUpdate(
@@ -25,10 +35,11 @@ export const updateTestimonial = (req, res) => {
     req.body,
     { new: true },
     (err, docs) => {
-      if (err || !docs) {
-        return err
-          ? responseHandler.error(res)
-          : responseHandler.error(res, 'Testimonial not found', 404);
+      if (err) {
+        throw new Error(err);
+      }
+      if (!docs) {
+        return responseHandler.error(res, 'Testimonial not found!', 404);
       }
       return responseHandler.success(res, docs);
     }
@@ -36,10 +47,11 @@ export const updateTestimonial = (req, res) => {
 };
 export const deleteTestimonial = (req, res) => {
   Testimonial.findByIdAndRemove(req.params.id, (err, doc) => {
-    if (err || !doc) {
-      return err
-        ? responseHandler.error(res)
-        : responseHandler.error(res, 'Testimonial not found', 404);
+    if (err) {
+      throw new Error(err);
+    }
+    if (!docs) {
+      return responseHandler.error(res, 'Testimonial not found!', 404);
     }
     // eslint-disable-next-line no-underscore-dangle
     return responseHandler.success(res, { _id: doc._id });
