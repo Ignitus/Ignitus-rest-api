beac11b6eceef0f39f48e1e7f8c4be669317bab1
Rawat <divyanshu.r46956@gmail.com>
2020-03-14 00:15:13 +0100

    Regenerating new keys due to breach 🤷‍♂️.

--- api/Configuration/config.js
+++ api/Configuration/config.js
@@ -1,10 +1,9 @@
 export const config = {
-  secretKey: '3970-582578-4263-184729',
-  mongoUrl: 'mongodb://ig-migration:migration12345@ds147566.mlab.com:47566/ignitus-migration',
-  hashingType: 'md5',
-  hashingDigest: 'hex',
-  privateVapidEmail: 'divyanshu.r46956@gmail.com',
-  publicVapidKey:
-    'BMRATagxJMbZdcX-a-lwaMC93vL7-Kmr3trsTc_g0HJy6C6UGCRMSKfxzJUOjAsXt3W5Ov7t_a8J9y9qROw9mFE',
-  privateVapidKey: 'NY_nxUlraMagPhmrwco8C-z0QH5xslPSAXP-cFk7cDM',
+  secretKey: process.env.SECRET_KEY,
+  mongoUrl: process.env.DATABASE_URI,
+  hashingType: process.env.HASHING_TYPE,
+  hashingDigest: process.env.HASHING_DIGEST,
+  privateVapidEmail: process.env.PVT_VAPID_EMAIL,
+  publicVapidKey: process.env.PUBLIC_VAPID_KEY,
+  privateVapidKey: process.env.PRIVATE_VAPID_KEY
 };
