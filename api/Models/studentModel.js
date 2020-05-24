a88815963187c40942568b1583cee9447e71842e
Rawat <divyanshu.r46956@gmail.com>
2020-05-24 16:06:21 +0200

    StudentModel renovated. 🎯

--- api/Models/studentModel.js
+++ api/Models/studentModel.js
@@ -1,72 +1,10 @@
 import mongoose from 'mongoose';
 
-const studentProfileSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/
-  },
-  username: {
-    type: String,
-    match: /^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/
-  },
-
-  about: { type: String },
-  address: { type: String },
-  skills: [String],
-  profilePicture: { type: String },
+const studentSchema = mongoose.Schema({
   curriculumVitae: { type: String },
-
-  socialNetworkLinks: {
-    facebook: { type: String },
-    github: { type: String },
-    linkedin: { type: String }
-  },
-  employment: {
-    current: { type: String },
-    openForOpportunities: { type: Boolean }
-  },
-  education: {
-    university: { type: String },
-    department: { type: String },
-    graduationYear: { type: Number }
-  },
-  experience: [
-    {
-      company: { type: String },
-      position: { type: String },
-      startDate: { type: Number },
-      endDate: { type: Number },
-      description: { type: String }
-    }
+  savedOpportunity: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Opportunity' },
   ],
-  recommendations: [
-    {
-      received: [
-        {
-          by: { type: String },
-          content: { type: String }
-        }
-      ],
-      given: [
-        {
-          to: { type: String },
-          content: { type: String }
-        }
-      ]
-    }
-  ],
-  publications: [
-    {
-      name: { type: String },
-      startDate: { type: Number },
-      endDate: { type: Number },
-      description: { type: String }
-    }
-  ]
 });
 
-const Student = mongoose.model('student', studentProfileSchema);
-export default Student;
+export const Student = mongoose.model('student', studentSchema);
