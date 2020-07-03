aa29a82af5556f8f4ab72ee8b0b2a3b31ba71638
Dishebh Bhayana <dishebh27@gmail.com>
2020-07-04 00:47:33 +0530

    hard-coding env variables

--- api/Configuration/config.ts
+++ api/Configuration/config.ts
@@ -1,6 +1,8 @@
 import { ConfigType } from 'api/Types/configType';
 
 export const config: ConfigType = {
-  secretKey: process.env.SECRET_KEY || '',
-  mongoUrl: process.env.DATABASE_URI || '',
+  secretKey: process.env.SECRET_KEY || 'my-secret-key',
+  mongoUrl:
+    process.env.DATABASE_URI ||
+    'mongodb+srv://dishebh:12345@cluster0-zpzln.mongodb.net/Ignitus-rest-api?retryWrites=true&w=majority',
 };
